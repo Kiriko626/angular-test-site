58d49f8e17d5624de00fb8b56ee19dbe8e7c3c13
Kiriko <kiriko626@gmail.com>
2019-01-17 13:57:08 +0500

    First commit

---
+++ src/app/users/user-single/user-single.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import {UserService} from "../../core/services/user.service";
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-user-single',
+  template: `
+      <section class="section">
+        <div class="container">
+          
+          <div class="card" *ngIf="user">
+            <img [src]="user.avatar_url">
+            <h2>{{ user.login }}</h2>
+          </div>
+        </div>
+      </section>
+  `,
+  styles: []
+})
+export class UserSingleComponent implements OnInit {
+  user;
+
+  constructor(private userService: UserService,
+              private route:ActivatedRoute) { }
+
+  ngOnInit() {
+
+    this.route.params.subscribe(params =>{
+      const username = params['username'];
+
+      this.userService
+          .getUser(username)
+          .subscribe(user => this.user = user);
+    });
+
+
+  }
+
+}
